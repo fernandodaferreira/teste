35fa8fcb7b2d96dab16d56a9488f95ef1e90c007
fernandodaferreira <43758998+fernandodaferreira@users.noreply.github.com>
2025-08-14 19:49:26 -0300

    Update ListGroupOrdersByBrandStore.sql

--- ListGroupOrdersByBrandStore.sql
+++ ListGroupOrdersByBrandStore.sql
@@ -1,6 +1,6 @@
-select count(o.order_id) as qtd_brand, b.brand_id, s.store_id  from orders as o 
+select count(*) as qtd_orders, b.brand_id, s.store_id  from orders as o 
 inner join order_items as oi on oi.order_id = o.order_id
 inner join products as p on p.product_id = oi.product_id
 inner join brands as b on b.brand_id = p.brand_id
 inner join stores as s on s.store_id = o.store_id
-group by o.order_id, b.brand_id, s.store_id
+group by b.brand_id, s.store_id
